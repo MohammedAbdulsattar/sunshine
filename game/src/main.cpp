cb9b95a82487fe81a545000eb58b3e16f4214258
Mohammed Abdulsattar <88753042+MohammedAbdulsattar@users.noreply.github.com>
2023-06-19 15:54:43 -0400

    Lab 4 - Part 1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,17 +1,71 @@
+#include <iostream>
 #include "rlImGui.h"
+#include "raymath.h"
+#include "raylib.h"
+//#include "Math.h"
+#include <vector>
+
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+enum class Tile
+{
+    Floor = 0, // Floor tiles can be walked on
+    Wall, // Wall tiles cannot be walked on
+    Count // number of Tile types (i.e. 2)
+};
+
+#define MAP_WIDTH 16 // rows (down from origin or start)
+#define MAP_HEIGHT 8 // columns (across from origin or start)
+
+class Tilemap
+{
+private:
+    Tile tiles[MAP_WIDTH][MAP_HEIGHT];
+
+    // 2D vector of tiles (just IDs storing which type it is)
+    // outer vector is to pick a column, inner vector is to pick a row from the column,
+    // so to get to tile at xy position (0,6) across tiles [0][6]
+    std::vector<std::vector<Tile>> tiles_vector; // Vector of Tiles - 2D dimension grid matrix of objects
+    int width = 0; // number of columns
+    int height = 0; // number of rows in each column
+};
+
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetTargetFPS(60);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine"); // Create Window
+    SetTargetFPS(60); // Maximum Target FPS
+
+    rlImGuiSetup(true); // Setup GUI Init
+    bool useGUI = false; // bool check to see if GUI is being used
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
+
+        float deltaTime = GetFrameTime(); // Get time in seconds for last frame drawn (delta time)
+
+        DrawRectangle(970, 10, 300, 30, { 200, 150, 200, 255 });
+        DrawText("Press ~ to open/close GUI", 980, 15, 20, WHITE); // GUI Instructions
+
+        if (IsKeyPressed(KEY_GRAVE))
+        {
+            useGUI = !useGUI; // Flip trigger for GUI Render when Tilde (~) Key is pressed
+        }
+
+        if (useGUI) // GUI window Open
+        {
+            rlImGuiBegin(); // Start GUI
+
+            ImGui::SliderFloat("Lab 4 Tilemap GUI", &deltaTime, 0, SCREEN_WIDTH, "%.3f", 0); // Slider for the 
+
+            rlImGuiEnd(); // End GUI
+        }
+
+
+
         EndDrawing();
     }
 
